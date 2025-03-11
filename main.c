016a8d5a170a7ae554226ed425e5eb803889ff50
James Cathcart <cathcart.jm@gmail.com>
2025-03-11 12:49:50 -0600

    start here

--- main.c
+++ main.c
@@ -2,21 +2,6 @@
 
 int main(void) {
 
-    char user_input = NULL;
-    int keep_going = 1;
-
-    printf("LCS Warriors Computer Club\n");
-    pritnf("-----------------------------\n");
-    printf("h - Hello World!\n");
-    printf("q - quit\n\n");
-    printf("$ ");
-
-    scanf("%s", user_input);
-
-    if (user_input == 'h') {
-        printf("Hello World!\n");
-    } else if (user_input == 'q') {
-        keep_going = 0;
-    }
+    
 
 }
