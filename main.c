94796388fe08d2382ea05ddbe58df47bb244be4d
James Cathcart <cathcart.jm@gmail.com>
2025-03-11 12:52:11 -0600

    user input example complete

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 
 int main(void) {
 
-    
+    char user_input = ' ';
+
+    printf("LCS Warriors Computer Club\n");
+    printf("-----------------------------\n");
+    printf("h - Hello World!\n");
+    printf("q - quit\n\n");
+    printf("$ ");
+
+    scanf("%s", &user_input);
+
+    if (user_input == 'h') {
+        printf("Hello World!\n");
+    }
 
 }
